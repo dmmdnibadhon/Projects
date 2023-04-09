43d8a35da1c15a24ace831e4070f8316782cbe41
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-04-08 19:02:57 -0700

    Projects

---
+++ Project-3/js/script.js
@@ -0,0 +1,10 @@
+// Filter-plugin start
+var mixer = mixitup('.filter_plugin');
+// Filter-plugin end
+
+
+// Progressbar part start
+$(document).ready(function(){
+    $(".progress-bar").ProgressBar();
+  });
+// Progressbar part end
